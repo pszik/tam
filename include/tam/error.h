22091e8f4a5028f43cfdc574ca9e1433223f8924
Ian Knight <ian.knight2@nottingham.ac.uk>
2025-04-17 09:59:11 +0100

    Add error messages and better error reporting

--- include/tam/error.h
+++ include/tam/error.h
@@ -1,6 +1,8 @@
 #ifndef TAM_ERROR_H__
 #define TAM_ERROR_H__
 
+#include <tam/tam.h>
+
 typedef enum TamError {
     OK,
     ErrFileNotFound,
@@ -11,5 +13,26 @@ typedef enum TamError {
     ErrStackOverflow,
     ErrStackUnderflow,
 } TamError;
+
+static const char *errorMessage(TamError Err) {
+    switch (Err) {
+    case OK:
+        return "";
+    case ErrFileNotFound:
+        return "input file not found";
+    case ErrFileLength:
+        return "input file was too long or contained incomplete instructions";
+    case ErrFileRead:
+        return "there was a problem while reading the input file";
+    case ErrCodeAccessViolation:
+        return "code access violation";
+    case ErrDataAccessViolation:
+        return "data access violation";
+    case ErrStackOverflow:
+        return "stack overflow";
+    case ErrStackUnderflow:
+        return "stack underflow";
+    }
+}
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <tam/error.h>
 #include <tam/tam.h>
 
 int main(int argc, const char **argv) {
     int ErrCode;
     TamEmulator Emulator;
 
-    ErrCode = loadProgram(&Emulator, argv[1]);
-    if (ErrCode) {
-        fprintf(stderr, "failed to load program (%d)\n", ErrCode);
+    if ((ErrCode = loadProgram(&Emulator, argv[1]))) {
+        fprintf(stderr, "%s\n", errorMessage(ErrCode));
         return ErrCode;
     }
 
     Instruction Instr;
     while (1) {
         if ((ErrCode = fetchDecode(&Emulator, &Instr))) {
-            fprintf(stderr, "failed to fetch instruction (%d)\n", ErrCode);
+            fprintf(stderr, "%s at loc %02x\n", errorMessage(ErrCode),
+                    Emulator.Registers[CP]);
             return ErrCode;
         }
 
@@ -23,7 +24,8 @@ int main(int argc, const char **argv) {
         }
 
         if ((ErrCode = execute(&Emulator, Instr))) {
-            fprintf(stderr, "execution error (%d)\n", ErrCode);
+            fprintf(stderr, "%s at loc %02x\n", errorMessage(ErrCode),
+                    Emulator.Registers[CP] - 1);
             return ErrCode;
         }
     }
